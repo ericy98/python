8a1acfe67388c815e7fc39c483fc0b1484b31958
Eric Yarbrough <eric.yarbrough@utexas.edu>
2021-01-07 11:38:25 -0600

    loops with lists

---
+++ Chap4/loopsWithLists.py
@@ -0,0 +1,6 @@
+# for i in range(4):
+#     print(i)
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+for i in range(len(supplies)):
+    print('INDEX ' + str(i) + ' in supplies is: ' + supplies[i])
