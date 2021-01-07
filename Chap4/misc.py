14182d243e3a1886a29c858c6e0a97bb6d7610b9
Eric Yarbrough <eric.yarbrough@utexas.edu>
2021-01-07 12:27:02 -0600

    enumerate + random/shuffle

---
+++ Chap4/misc.py
@@ -0,0 +1,9 @@
+# enumerate() lists
+# supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+# for index, item in enumerate(supplies):
+#     print('INDEX ' + str(index) + ' in supplies is: ' + item)
+
+# random.choice() in REPL
+# import random
+# pets = ['Dog', 'Cat', 'Moose']
+# random.choice(pets)
