312c0f4217c735544eb85e76e279175c97f4f391
Eric Yarbrough <eric.yarbrough@utexas.edu>
2021-02-01 14:21:06 -0600

    set main loop and print currentCells

--- Chap4/conwaysGame.py
+++ Chap4/conwaysGame.py
@@ -3,7 +3,7 @@ import random, time, copy
 WIDTH = 60
 HEIGHT = 20
 
-#Create a list of list for the cells:
+# Create a list of list for the cells:
 nextCells = []
 for x in range(WIDTH):
     column = [] # Create a new column
@@ -13,3 +13,13 @@ for x in range(WIDTH):
         else:
             column.append('') # Add a dead cell
     nextCells.append(column) # nextCells is a list of column lists
+
+while True: # Main program loop
+    pring('\n\n\n\n\n') # Seperate each step with newlines.
+    currentCells = copy.deepcopy(nextCells)
+
+    # Pring currentCells on the screen:
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            pring(currentCells[x][y], end=') # Print the # or space
+        print() # Print newline at end of row
