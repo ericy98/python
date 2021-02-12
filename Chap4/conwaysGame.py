f73088d7168a5d7613cda8be99f3a9ff51b6e808
Eric Yarbrough <eric.yarbrough@utexas.edu>
2021-02-12 15:59:39 -0600

    count living neighbors-conway game

--- Chap4/conwaysGame.py
+++ Chap4/conwaysGame.py
@@ -23,3 +23,32 @@ while True: # Main program loop
         for x in range(WIDTH):
             pring(currentCells[x][y], end=') # Print the # or space
         print() # Print newline at end of row
+
+    # Calculate next step's cells
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates:
+            # `%WIDTH` ensures leftCoord is always between 0 and WIDTH -1
+            leftCoord = (x-1) % WIDTH
+            rightCoord = (x+1) % WIDTH
+            aboveCoord = (y-1) % HEIGHT
+            belowCoord = (y+1) % HEIGHT
+
+            #Count # of living neighbors:
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] =='#':
+                numNeighbors += 1 # Top-left neighbor is alive.
+            if currentCells[x][aboveCoord] == '#':
+                numNeighbors += 1 # Top neighbor is alive
+            if currentCells[rightCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-right alive
+            if currentCells[leftCoord][y] == '#':
+                numNeighbors += 1 # Left alive
+            if currentCells[rightCoord][y] == '#':
+                numNeighbors += 1 # Right alive
+            if currentCells[leftCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-left alive
+            if currentCells[x][belowCoord] == '#':
+                numNeighbors += 1 # Bottom alive
+            if currentCells[rightCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-right alive
